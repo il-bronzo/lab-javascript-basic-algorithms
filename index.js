3941870c314bebb9afab256dffabc1ab79229a0f
Sven Andersson <sven@andersson.es>
2024-09-21 12:37:28 +0200

    Iteration 2 completed

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ console.log(`The driver's name is ${hacker1}`);
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
+const driver = "Andrea";
+const navigator = "Sven";
+//const driverLeng
 
+if (driver.length > navigator.length) {
+    console.log(`The driver has the longest name, it has ${driver.length} characters.`);
+} else if (driver.length < navigator.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters.`);
+} else if (driver.length === navigator.length) {
+    console.log(`Wow, you both have equally long names, ${driver.length} characters!`);
+    
+}∑
 
 // Iteration 3: Loops
